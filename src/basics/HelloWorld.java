7e221266800d12ef42456325eb9e2910f7dc4975
LaiDungLam <40560677+LaiDungLam@users.noreply.github.com>
2018-09-30 07:16:32 +0700

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main (String[] args) {
 		System.out.println("Hello World");
 		System.out.println("I am adding codes to my projects"); 
+		System.out.println("adding codes from git hub");
 	}
 }
