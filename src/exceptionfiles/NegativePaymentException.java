f1a64e2a4b47db495b8030a8e09c58dd5324e41d
LaiDungLam <40560677+LaiDungLam@users.noreply.github.com>
2018-09-30 09:56:25 +0700

    Update NegativePaymentException.java

--- src/exceptionfiles/NegativePaymentException.java
+++ src/exceptionfiles/NegativePaymentException.java
@@ -1,13 +1,16 @@
 package exceptionfiles;
 
 public class NegativePaymentException extends Exception {
+	//define class variable 
 	double payment; 
-	
+	//1. constructor that takes the value that would throw the exception
+	//2. assign value of local variable to class variable 
 	public NegativePaymentException(double payment) {
 		this.payment = payment; 
 		System.out.println("ERROR: negative payment"); 
 	}
 	
+	//3. Override the toString() method to include the specific exception message
 	public String toString() {
 		return "Error: cannot take negative payment " + payment; 
 	}
